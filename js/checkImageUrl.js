79a49177512c42aeffa2bcd7244c178d26260c4b
Nattana <nattana@acommerce.asia>
2024-12-17 15:17:50 +0700

    remove unuse files

--- js/checkImageUrl.js
+++
@@ -1,8 +0,0 @@
-// checkImageUrl.js
-export const checkImageURL = (url) => {
-    if (!url) return false
-    else {
-        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
-        return pattern.test(url);
-    }
-};
